9d8dddf2fde1ff3cc2351fed9cba8af1c8622b66
HoYoungKim <mo02@outlook.kr>
2017-04-17 22:16:59 +0900

    TEST

--- model/sequelize.js
+++ model/sequelize.js
@@ -1,12 +1,24 @@
 const Sequelize = require('sequelize');
-const db_config  = require('../db-config.json');
+var env       = process.env.NODE_ENV || "development";
+
+var prodConfig = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_TYPE,
+  pool: { "max": 5, "min": 0, "idle": 10000 }
+};
+
+var db_config    = env == "production" ? prodConfig : require('../db-config.json');;
 /* Sequelize 연결*/
 const sequelize = new Sequelize(
-  db_config.database,
-  db_config.user,
-  db_config.password,
+  process.env.DB_NAME||db_config.database,
+  process.env.DB_USER||db_config.user,
+  process.env.DB_PASSWORD || db_config.password, env == "production" ? prodConfig : db_config,
   {
-    'host' : db_config.host,
+    'host' : process.env.DB_HOST||db_config.host,
     'dialect' : 'mysql'
   }
 );
